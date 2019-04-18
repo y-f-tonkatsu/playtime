569bd66511273a085dc3825931182612ba4e97fd
Y_F_TONKATSU <machbuilders@gmail.com>
2019-04-19 00:10:36 +0900

    Create a skeleton

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
-var express = require('express');
-var router = express.Router();
+const _ = require('lodash');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const express = require('express');
+const router = express.Router();
+
+const fs = require("fs");
+const path = require("path");
+const url = require('url');
+
+var worksJson;
+
+var respond = function (req, res, next, id) {
+
+    if (!worksJson) {
+        worksJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../public/works/works.json'), 'utf8'));
+    }
+
+    let stage;
+
+    let base = '';
+    let title, keyowords, description, ogUrl, thumb;
+
+    let index = false;
+    if (!stage) {
+        title = 'Y.F.T のプレイタイム'
+        description = '山田 F とんかつの個人的な楽しみのためのサイトです。';
+        ogUrl = base;
+        thumb = base + "works/absphoto/000_maru.jpg";
+        index = true;
+        stage = worksJson[0];
+        id = 0;
+        current = "";
+    } else {
+        title = stage.title;
+        description = stage.description;
+        ogUrl = current;
+        thumb = base + "works" + stage.path;
+    }
+
+
+    res.render('index', {
+        og_title: title,
+        og_description: description,
+        og_url: ogUrl,
+        og_thumb: thumb,
+        baseUrl: base,
+        currentUrl: current,
+        worksUrl: base + "/works",
+        works: worksJson,
+        stage: stage,
+        index: index,
+        init: id,
+        dirname: __dirname
+    });
+
+};
+
+router.get('/', function (req, res, next) {
+    respond(req, res, next, 0);
 });
+
+router.get('/:id/', function (req, res, next) {
+    var id = parseInt(req.params["id"]);
+    respond(req, res, next, id);
+});
+
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,3 +2,58 @@ const React = require('react');
 const ReactDom = require('react-dom');
 const style = require('./index.css');
 
+const Router = require('react-router-dom').BrowserRouter;
+const Route = require('react-router-dom').Route;
+const Link = require('react-router-dom').Link;
+
+const path = require('path');
+
+const works = require('../public/works/works.json');
+
+const _ = require('lodash');
+
+class Doc extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <Route path={'/:id'} component={Stage}/>
+            </Router>
+        );
+    }
+}
+
+class Stage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const params = props.match.params;
+        const work = _.find(works, (currentWork) => {
+            return currentWork.id.toString() === params.id.toString();
+        });
+        this.state = {
+            'work': work,
+        }
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <h1>Stage</h1>
+                    <img src={path.join('works/', this.state.work.path)}/>
+                    <article>{this.state.work.description}</article>
+                </div>
+            </Router>);
+    }
+}
+
+window.onload = function () {
+    ReactDom.render(
+        <Doc/>,
+        document.getElementById('root')
+    );
+}
