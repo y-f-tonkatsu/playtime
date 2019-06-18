355375113ead2a00a652a72679c210e09086af08
Y_F_TONKATSU <machbuilders@gmail.com>
2019-06-19 00:17:35 +0900

    Update kyoai ex

--- public/_ex/kyoai_v001.js
+++ public/_ex/kyoai_v001.js
@@ -2,26 +2,10 @@
 
 var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
-lib.ssMetadata = [
-		{name:"kyoai_v001_atlas_", frames: [[0,22,16,16],[0,0,20,20]]}
-];
+lib.ssMetadata = [];
 
 
 // symbols:
-
-
-
-(lib.CachedTexturedBitmap_1 = function() {
-	this.initialize(ss["kyoai_v001_atlas_"]);
-	this.gotoAndStop(0);
-}).prototype = p = new cjs.Sprite();
-
-
-
-(lib.CachedTexturedBitmap_2 = function() {
-	this.initialize(ss["kyoai_v001_atlas_"]);
-	this.gotoAndStop(1);
-}).prototype = p = new cjs.Sprite();
 // helper functions:
 
 function mc_symbol_clone() {
@@ -45,18 +29,16 @@ function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
 	this.initialize(mode,startPosition,loop,{});
 
 	// レイヤー_1
-	this.instance = new lib.CachedTexturedBitmap_1();
-	this.instance.parent = this;
-	this.instance.setTransform(-4,-4,0.5,0.5);
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#9D1C52").s().p("AgdAoIAAgKIgKAAIAAg7IAKAAIAAgKIA7AAIAAAKIAKAAIAAA7IgKAAIAAAKg");
 
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
 
 	// レイヤー_2
-	this.instance_1 = new lib.CachedTexturedBitmap_2();
-	this.instance_1.parent = this;
-	this.instance_1.setTransform(-5,-5,0.5,0.5);
+	this.shape_1 = new cjs.Shape();
+	this.shape_1.graphics.f("rgba(157,28,82,0.498)").s().p("AgnAyIAAgKIgKAAIAAhPIAKAAIAAgKIBPAAIAAAKIAKAAIAABPIgKAAIAAAKg");
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(1));
+	this.timeline.addTween(cjs.Tween.get(this.shape_1).wait(1));
 
 }).prototype = getMCSymbolPrototype(lib.Pixel, new cjs.Rectangle(-5,-5,10,10), null);
 
@@ -84,7 +66,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance.setTransform(-24,16);
 	this.instance._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance).wait(19).to({_off:false},0).wait(31).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(19).to({_off:false},0).wait(51));
 
 	// Pixel_r_build
 	this.instance_1 = new lib.Pixel_r_build("synched",0,false);
@@ -92,7 +74,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_1.setTransform(0,-32);
 	this.instance_1._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(18).to({_off:false},0).wait(32).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(18).to({_off:false},0).wait(52));
 
 	// Pixel_r_build
 	this.instance_2 = new lib.Pixel_r_build("synched",0,false);
@@ -100,7 +82,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_2.setTransform(-8,16);
 	this.instance_2._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(18).to({_off:false},0).wait(32).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(18).to({_off:false},0).wait(52));
 
 	// Pixel_r_build
 	this.instance_3 = new lib.Pixel_r_build("synched",0,false);
@@ -108,7 +90,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_3.setTransform(7,-8);
 	this.instance_3._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(18).to({_off:false},0).wait(32).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(18).to({_off:false},0).wait(52));
 
 	// Pixel_r_build
 	this.instance_4 = new lib.Pixel_r_build("synched",0,false);
@@ -116,7 +98,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_4.setTransform(24,24);
 	this.instance_4._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(18).to({_off:false},0).wait(32).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_4).wait(18).to({_off:false},0).wait(52));
 
 	// Pixel_r_build
 	this.instance_5 = new lib.Pixel_r_build("synched",0,false);
@@ -124,7 +106,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_5.setTransform(8,16);
 	this.instance_5._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(16).to({_off:false},0).wait(34).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_5).wait(16).to({_off:false},0).wait(54));
 
 	// Pixel_r_build
 	this.instance_6 = new lib.Pixel_r_build("synched",0,false);
@@ -132,7 +114,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_6.setTransform(-24,24);
 	this.instance_6._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(16).to({_off:false},0).wait(34).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_6).wait(16).to({_off:false},0).wait(54));
 
 	// Pixel_r_build
 	this.instance_7 = new lib.Pixel_r_build("synched",0,false);
@@ -140,7 +122,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_7.setTransform(-24,32);
 	this.instance_7._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(15).to({_off:false},0).wait(35).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_7).wait(15).to({_off:false},0).wait(55));
 
 	// Pixel_r_build
 	this.instance_8 = new lib.Pixel_r_build("synched",0,false);
@@ -148,7 +130,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_8.setTransform(-16,16);
 	this.instance_8._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_8).wait(14).to({_off:false},0).wait(36).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_8).wait(14).to({_off:false},0).wait(56));
 
 	// Pixel_r_build
 	this.instance_9 = new lib.Pixel_r_build("synched",0,false);
@@ -156,7 +138,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_9.setTransform(16,8);
 	this.instance_9._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_9).wait(14).to({_off:false},0).wait(36).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_9).wait(14).to({_off:false},0).wait(56));
 
 	// Pixel_r_build
 	this.instance_10 = new lib.Pixel_r_build("synched",0,false);
@@ -164,7 +146,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_10.setTransform(-16,8);
 	this.instance_10._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_10).wait(12).to({_off:false},0).wait(38).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_10).wait(12).to({_off:false},0).wait(58));
 
 	// Pixel_r_build
 	this.instance_11 = new lib.Pixel_r_build("synched",0,false);
@@ -172,7 +154,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_11.setTransform(16,0);
 	this.instance_11._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_11).wait(11).to({_off:false},0).wait(39).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_11).wait(11).to({_off:false},0).wait(59));
 
 	// Pixel_r_build
 	this.instance_12 = new lib.Pixel_r_build("synched",0,false);
@@ -180,7 +162,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_12.setTransform(-8,-16);
 	this.instance_12._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_12).wait(10).to({_off:false},0).wait(40).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_12).wait(10).to({_off:false},0).wait(60));
 
 	// Pixel_r_build
 	this.instance_13 = new lib.Pixel_r_build("synched",0,false);
@@ -188,7 +170,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_13.setTransform(24,16);
 	this.instance_13._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_13).wait(9).to({_off:false},0).wait(41).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_13).wait(9).to({_off:false},0).wait(61));
 
 	// Pixel_r_build
 	this.instance_14 = new lib.Pixel_r_build("synched",0,false);
@@ -196,7 +178,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_14.setTransform(7,-16);
 	this.instance_14._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_14).wait(9).to({_off:false},0).wait(41).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_14).wait(9).to({_off:false},0).wait(61));
 
 	// Pixel_r_build
 	this.instance_15 = new lib.Pixel_r_build("synched",0,false);
@@ -204,7 +186,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_15.setTransform(16,16);
 	this.instance_15._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_15).wait(8).to({_off:false},0).wait(42).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_15).wait(8).to({_off:false},0).wait(62));
 
 	// Pixel_r_build
 	this.instance_16 = new lib.Pixel_r_build("synched",0,false);
@@ -212,7 +194,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_16.setTransform(-8,-8);
 	this.instance_16._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_16).wait(7).to({_off:false},0).wait(43).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_16).wait(7).to({_off:false},0).wait(63));
 
 	// Pixel_r_build
 	this.instance_17 = new lib.Pixel_r_build("synched",0,false);
@@ -220,7 +202,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_17.setTransform(0,16);
 	this.instance_17._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_17).wait(7).to({_off:false},0).wait(43).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_17).wait(7).to({_off:false},0).wait(63));
 
 	// Pixel_r_build
 	this.instance_18 = new lib.Pixel_r_build("synched",0,false);
@@ -228,7 +210,7 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_18.setTransform(24,32);
 	this.instance_18._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_18).wait(6).to({_off:false},0).wait(44).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_18).wait(6).to({_off:false},0).wait(64));
 
 	// Pixel_r_build
 	this.instance_19 = new lib.Pixel_r_build("synched",0,false);
@@ -236,14 +218,14 @@ p.nominalBounds = new cjs.Rectangle(-5,-185,10,190);
 	this.instance_19.setTransform(-16,0);
 	this.instance_19._off = true;
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_19).wait(4).to({_off:false},0).wait(46).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_19).wait(4).to({_off:false},0).wait(66));
 
 	// Pixel_r_build
 	this.instance_20 = new lib.Pixel_r_build("synched",0,false);
 	this.instance_20.parent = this;
 	this.instance_20.setTransform(0,-24);
 
-	this.timeline.addTween(cjs.Tween.get(this.instance_20).wait(50).to({startPosition:7},0).to({_off:true},1).wait(19));
+	this.timeline.addTween(cjs.Tween.get(this.instance_20).wait(70));
 
 }).prototype = p = new cjs.MovieClip();
 p.nominalBounds = new cjs.Rectangle(-29,-217,58,254);
@@ -270,9 +252,7 @@ lib.properties = {
 	fps: 8,
 	color: "#FFFFFF",
 	opacity: 1.00,
-	manifest: [
-		{src:"images/kyoai_v001_atlas_.png?1560870859611", id:"kyoai_v001_atlas_"}
-	],
+	manifest: [],
 	preloads: []
 };
 
