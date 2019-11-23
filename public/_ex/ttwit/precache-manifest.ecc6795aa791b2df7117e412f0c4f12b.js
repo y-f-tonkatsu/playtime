3333c109b4fb81cd600b5515290d0a4534e37ba4
yft <machbuilders@gmail.com>
2019-11-23 16:28:13 +0900

    Add ex

---
+++ public/_ex/ttwit/precache-manifest.ecc6795aa791b2df7117e412f0c4f12b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af907d64f8cef43cce6717cb97619b72",
+    "url": "/playtime/_ex/ttwit/index.html"
+  },
+  {
+    "revision": "e66db464e65368b5ee36",
+    "url": "/playtime/_ex/ttwit/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "97eac871fb36352cda42",
+    "url": "/playtime/_ex/ttwit/static/js/2.7e7f729f.chunk.js"
+  },
+  {
+    "revision": "e66db464e65368b5ee36",
+    "url": "/playtime/_ex/ttwit/static/js/main.7cfd104e.chunk.js"
+  },
+  {
+    "revision": "903794aa00b6b7dd364f",
+    "url": "/playtime/_ex/ttwit/static/js/runtime-main.c77cd5f0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/playtime/_ex/ttwit/static/media/logo.25bf045c.svg"
+  }
+]);
